66b838e9772960a48f26275420bb9e03606d3575
K Müller <muellerk2006@gmail.com>
2026-01-23 14:27:11 +0100

    No longer end program on error case

--- extra/sensor_module/sensor_module.c
+++ extra/sensor_module/sensor_module.c
@@ -4,19 +4,16 @@ void check_status(unsigned char status) {
     // check Error
     if (status & (0x01 << 7)) {
         printf("General error.\n");
-        return;
     }
 
     // check overtemperature
     if (status & (0x01 << 6)) {
         printf("Overtemperature.\n");
-        return;
     }
 
     // check undervoltage
     if (status & (0x01 << 5)) {
         printf("Undervoltage.\n");
-        return;
     }
 
     unsigned char measurement = status & 0x03;
