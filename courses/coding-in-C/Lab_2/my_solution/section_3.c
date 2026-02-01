0cd7e4e6f95b1d3c1ec0d28177faf09a9f927353
K Müller <muellerk2006@gmail.com>
2026-02-01 13:54:10 +0100

    Print horizontal line divider

--- courses/coding-in-C/Lab_2/my_solution/section_3.c
+++ courses/coding-in-C/Lab_2/my_solution/section_3.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 
 void print_column_letters() {
-    printf("  ");
+    printf("   ");
 
     for (int column = 0; column < 8; column++) {
         char letter = 'A' + column;
-        printf(" %c ", letter);
+        printf("%c   ", letter);
     }
 
     printf("\n");
 }
 
+void print_horizontal_line() {
+    printf(" ");
+
+    for (int column = 0; column < 8; column++) {
+        printf("+---");
+    }
+
+    printf("+\n");
+}
+
 void print_chessboard() {
     print_column_letters();
+    print_horizontal_line();
 }
 
 int main() {
