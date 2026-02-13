3581ef4a1888654264d211b559930df1f60ea794
K Müller <muellerk2006@gmail.com>
2026-02-13 11:04:10 +0100

    Implement section_2

--- courses/coding-in-C/Lab_5/my_solution/section_2.c
+++ courses/coding-in-C/Lab_5/my_solution/section_2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -6,7 +7,21 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    printf("argc: %d\n", argc);
+    printf("Using array indexing:\n");
+    for (int i = 1; i < argc; i++) {
+        printf("Index: %d\n", i);
+        printf("Address: %p\n", argv[i]);
+        printf("Content: %s\n", argv[i]);
+        printf("Length: %ld\n\n", strlen(argv[i]));
+    }
+
+    printf("Using pointer indexing:\n");
+    for (int i = 1; i < argc; i++) {
+        printf("Index: %d\n", i);
+        printf("Address: %p\n", *(argv + i));
+        printf("Content: %s\n", *(argv + i));
+        printf("Length: %ld\n\n", strlen(*(argv + i)));
+    }
 
     return 0;
 }
