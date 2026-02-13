56210331e37b7a5e9d54406a5603df1e0bf7b3d2
K Müller <muellerk2006@gmail.com>
2026-02-13 10:52:45 +0100

    Create C file for section 1

---
+++ courses/coding-in-C/Lab_5/my_solution/section_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 10;
+    int *p = &x;
+    int **pp = &p;
+
+    // TODO: complete the output statements
+    printf("x = %d\n", ???);
+    printf("x via p = %d\n", ???);
+    printf("x via pp = %d\n", ???);
+
+    return 0;
+}
